3d3e773839a0d7ea4a130299ed985f854fdc534d
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2022-11-06 20:51:46 +0300

    add `ResponseWriter`

---
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/response/ResponseWriter.java
@@ -0,0 +1,40 @@
+package ru.akirakozov.sd.refactoring.response;
+
+import ru.akirakozov.sd.refactoring.database.Product;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResponseWriter {
+
+    private final HttpServletResponse response;
+
+    public ResponseWriter(HttpServletResponse response) {
+        this.response = response;
+    }
+
+    public void writeOk() {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    public void writeHeader() throws IOException {
+        response.getWriter().println("<html><body>");
+    }
+
+    public void writeFooter() throws IOException {
+        response.getWriter().println("</body></html>");
+    }
+
+    public void writeHeading(String heading) throws IOException {
+        response.getWriter().println("<h1>" + heading + "</h1>");
+    }
+
+    public void println(String line) throws IOException {
+        response.getWriter().println(line);
+    }
+
+    public void writeProduct(Product product) throws IOException {
+        response.getWriter().println(product.getName() + "\t" + product.getPrice() + "</br>");
+    }
+}
--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -2,73 +2,68 @@ package ru.akirakozov.sd.refactoring.servlet;
 
 import ru.akirakozov.sd.refactoring.database.Database;
 import ru.akirakozov.sd.refactoring.database.Product;
+import ru.akirakozov.sd.refactoring.response.ResponseWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Optional;
 
 /**
  * @author akirakozov
  */
 public class QueryServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String command = request.getParameter("command");
 
-        if ("max".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h1>Product with max price: </h1>");
-            try {
-                Optional<Product> product = Database.getMostExpensiveProduct();
-                if (product.isPresent()) {
-                    response.getWriter().println(product.get().getName() + "\t" + product.get().getPrice() + "</br>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            doGetImpl(request, response);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doGetImpl(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException {
+        String command = request.getParameter("command");
+        ResponseWriter responseWriter = new ResponseWriter(response);
+
+        switch (command) {
+            case "max":
+                responseWriter.writeHeader();
+                responseWriter.writeHeading("Product with max price: ");
+                Optional<Product> maxProduct = Database.getMostExpensiveProduct();
+                if (maxProduct.isPresent()) {
+                    responseWriter.writeProduct(maxProduct.get());
                 }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            response.getWriter().println("</body></html>");
-        } else if ("min".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h1>Product with min price: </h1>");
-            try {
-                Optional<Product> product = Database.getCheapestProduct();
-                if (product.isPresent()) {
-                    response.getWriter().println(product.get().getName() + "\t" + product.get().getPrice() + "</br>");
+                responseWriter.writeFooter();
+                break;
+            case "min":
+                responseWriter.writeHeader();
+                responseWriter.writeHeading("Product with min price: ");
+                Optional<Product> minProduct = Database.getCheapestProduct();
+                if (minProduct.isPresent()) {
+                    responseWriter.writeProduct(minProduct.get());
                 }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            response.getWriter().println("</body></html>");
-        } else if ("sum".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("Summary price: ");
-            try {
-                response.getWriter().println(Database.getProductsSum());
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            response.getWriter().println("</body></html>");
-        } else if ("count".equals(command)) {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("Number of products: ");
-            try {
-                response.getWriter().println(Database.getProductsCount());
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-            response.getWriter().println("</body></html>");
-        } else {
-            response.getWriter().println("Unknown command: " + command);
+                responseWriter.writeFooter();
+                break;
+            case "sum":
+                responseWriter.writeHeader();
+                responseWriter.println("Summary price: ");
+                responseWriter.println(String.valueOf(Database.getProductsSum()));
+                responseWriter.writeFooter();
+                break;
+            case "count":
+                responseWriter.writeHeader();
+                responseWriter.println("Number of products: ");
+                responseWriter.println(String.valueOf(Database.getProductsCount()));
+                responseWriter.writeFooter();
+                break;
+            default:
+                responseWriter.println("Unknown command: " + command);
+                break;
         }
 
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+        responseWriter.writeOk();
     }
 }
